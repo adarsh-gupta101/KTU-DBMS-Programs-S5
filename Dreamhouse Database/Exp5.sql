4e7afd9ec817f1e763851da65ab553e2d23482dc
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2022-10-31 04:11:53 -0400

    Update Exp5.sql

--- Dreamhouse Database/Exp5.sql
+++ Dreamhouse Database/Exp5.sql
@@ -4,3 +4,12 @@
 UPDATE Staff SET salary= salary *1.03;
 
 UPDATE Staff SET salary= salary *1.05 Where position="Manager";
+
+UPDATE PropertyForRent SET rent= rent *1.05 ;
+
+Update Staff set position="Manager",salary=18000 where fName="David";
+
+Delete from Viewing where propertyNo ="PG4";
+
+Delete from Viewing ;
+
