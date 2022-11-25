620f96949752b8f9359f667f322f30f16766c1d9
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2022-11-25 00:10:03 -0500

    Update Expirement10DCL.sql

--- Dreamhouse Database/Expirement10DCL.sql
+++ Dreamhouse Database/Expirement10DCL.sql
@@ -10,3 +10,21 @@ FROM Staff s, PropertyForRent p WHERE s.staffNo = p.staffNo GROUP BY s.branchNo,
 
 
 2.1 Create a new MySQL user with a username and password using ‘CREATE USER’ command.
+
+
+    1. Create a new MySQL user with a username and password using ‘CREATE USER’ command.
+2.1 Create a new MySQL user with a username and password using ‘CREATE USER’ command.
+
+(
+OUTPUT -> WAS USING ONLINE COMPILER SO COULDNT RUN THIS COMMAND)
+
+    2. Grant the new user all privileges on ‘Branch’ table of ‘DreamHome’ schema.
+   GRANT ALL ON DreamHome.Branch59 TO 'adarsh'@'localhost';
+
+
+    3. Grant the new user read-only privileges on ‘PropertyForRent’ table of ‘DreamHome’ schema.
+       GRANT SELECT ON DreamHome.PropertyForRent59 TO 'adarsh'@'localhost';
+    4. Validate the privilege assignments with proper queries as the new user. (Write all queries and outputs used for validation)
+    5. Revoke all privileges given to the new user and validate the same.
+REVOKE ALL ON DreamHome.Branch59 FROM 'adarsh'@'localhost';
+REVOKE SELECT ON DreamHome.PropertyForRent59 FROM 'adarsh'@'localhost';
