a0678dc14af40a0c2540290b1c879efcdafdf696
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2022-11-24 23:44:05 -0500

    Update Expirement10DCL.sql

--- Dreamhouse Database/Expirement10DCL.sql
+++ Dreamhouse Database/Expirement10DCL.sql
@@ -1,1 +1,3 @@
 1. CREATE VIEW Manager3Staff AS SELECT * FROM Staff WHERE branchNo = 'B003';
+
+2. CREATE VIEW Staff3 AS SELECT staffNo, fName, IName, position, sex FROM Staff WHERE branchNo = 'B003';
