26d803183ef7e1475e802064b3beb8962fa42ab2
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2022-11-25 00:02:24 -0500

    Update Expirement10DCL.sql

--- Dreamhouse Database/Expirement10DCL.sql
+++ Dreamhouse Database/Expirement10DCL.sql
@@ -1,3 +1,12 @@
 1. CREATE VIEW Manager3Staff AS SELECT * FROM Staff WHERE branchNo = 'B003';
 
 2. CREATE VIEW Staff3 AS SELECT staffNo, fName, IName, position, sex FROM Staff WHERE branchNo = 'B003';
+
+3. CREATE VIEW Staffpropetiescount (branchNo, staffNo, cnt)
+AS SELECT s.branchNo, s.staffNo, COUNT(*)
+FROM Staff s, PropertyForRent p WHERE s.staffNo = p.staffNo GROUP BY s.branchNo, s.staffNo;
+
+
+
+
+2.1 Create a new MySQL user with a username and password using ‘CREATE USER’ command.
